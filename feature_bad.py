d91c53e446819dda85ac98f61886d51344c02960
Jedrzej Wiewiora <jedrzej.wiewiora@gmail.com>
2018-03-02 11:09:24 +0100

    smrut

---
+++ feature_bad.py
@@ -0,0 +1,9 @@
+# This program reads two numbers and prints their sum:
+# Can you change it so it can read and sum three numbers?
+
+a = int(input())
+b = int(input())
+c = int(input())
+def bb(arg):
+    return arg*arg
+print(a + b +c)
